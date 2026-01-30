4bba746fd88b2671c86fbf235ac7678da13c8856
sarthwa8 <sarthakvs10@gmail.com>
2026-01-30 14:05:54 +0530

    Revert to 8884 port

--- web-dashboard/src/App.jsx
+++ web-dashboard/src/App.jsx
@@ -10,9 +10,9 @@ import "./App.css";
 // ------------------------------------------------------------------
 // 🔒 FIREWALL BYPASS CONFIGURATION (HiveMQ Cloud)
 // ------------------------------------------------------------------
-// 1. Cluster URL (Keep 'wss://' and ':443/mqtt')
+// 1. Cluster URL (Keep 'wss://' and ':8884/mqtt')
 const CLUSTER_URL =
-  "wss://0ad1bd1bd95e47578dcf81d81b956924.s1.eu.hivemq.cloud:443/mqtt";
+  "wss://0ad1bd1bd95e47578dcf81d81b956924.s1.eu.hivemq.cloud:8884/mqtt";
 
 // 2. Your HiveMQ Cloud Credentials
 const MQTT_USERNAME = "Sarthak_Sukhral";
